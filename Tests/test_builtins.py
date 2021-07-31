3841c7e748cbf18d1deb09cf60bedf469decf789
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-07-31 21:03:48 +1000

    start a builtin test

---
+++ Tests/test_builtins.py
@@ -0,0 +1,85 @@
+def test_abs():
+    assert abs(1) == 1
+    assert abs(-1) == 1
+    assert abs(0.5) == 0.5
+
+
+def test_all():
+    assert all([True, False, True]) == False
+    assert all([False, False, False]) == False
+    assert all([1, 1, 1]) == True
+    assert all([True, True, True]) == True
+
+
+def test_any():
+    assert any([True, False, True]) == True
+    assert any([False, False, False]) == False
+    assert any([1, 1, 1]) == True
+    assert any([True, True, True]) == True
+
+
+def test_ascii():
+    assert ascii(1) == "1"
+    assert ascii(0.5) == "0.5"
+    assert ascii("hello") == "hello"
+
+
+def test_bin():
+    assert bin(3) == '0b11'
+    assert bin(-10) == '-0b1010'
+
+
+def test_bool():
+    assert bool(1) == True
+    assert bool(True) == True
+    assert bool(0) == False
+
+def test_bytearray():
+    assert bytearray([True, False]) == bytearray(b'\x01\x00')
+
+def test_bytes():
+    assert bytes(bytes("hello", 'utf-8')) == b'hello'
+    assert bytes(0x2) == b'\x00\x00'
+    assert bytes((True, False)) == b'\x01\x00'
+
+def test_callable():
+    assert callable(test_callable) == True
+    assert callable(1) == False
+    assert callable(bin) == True
+
+def test_chr():
+    assert chr(91) == '['
+    assert chr(True) == '\x01'
+    assert chr(23480) == '宸'
+
+def test_classmethod():
+    class F:
+        @classmethod
+        def foo(cls, n):
+            return cls
+
+    assert F.foo() == F
+
+def test_complex():
+    assert complex('1+2j') == (1+2j)
+    assert complex('1') == (1+0j)
+
+def test_dict():
+    assert dict([(1,0),(2,3),(3,4)]) == {1: 0, 2: 3, 3: 4}
+
+def test_dir():
+    assert 'upper' in dir('hello')
+
+def test_divmod():
+    assert divmod(1, 5) == (0, 1)
+    assert divmod(1, 5.) == (0.0, 1.0)
+    assert divmod(1., 5.) == (0.0, 1.0)
+    assert divmod(-1., 5.) == (-1.0, 4.0)
+
+def test_enumerate():
+    seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+    assert list(enumerate(seasons)) == [(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
+    assert list(enumerate(seasons, start=1)) == [(1, 'Spring'), (2, 'Summer'), (3, 'Fall'), (4, 'Winter')]
+
+def test_filter():
+    pass
