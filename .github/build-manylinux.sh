fe3ce1138ae4b1f9149bb698d82e80de46a8fc88
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-09-27 15:57:34 +1000

    Update manylinux script

--- .github/build-manylinux.sh
+++ .github/build-manylinux.sh
@@ -4,10 +4,10 @@ set -e -x
 cd /github/workspace/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/src/dotnet/shared/Microsoft.NETCore.App/6.0.0-rc.1.21451.13
 echo $LD_LIBRARY_PATH
-/opt/python/cp39-cp39/bin/pip install --upgrade --no-cache-dir pip
-/opt/python/cp39-cp39/bin/pip install auditwheel
-/opt/python/cp39-cp39/bin/pip install .
-/opt/python/cp39-cp39/bin/pip wheel . -w ./dist --no-deps
+/opt/python/cp310-cp310/bin/pip install --upgrade --no-cache-dir pip
+/opt/python/cp310-cp310/bin/pip install auditwheel
+/opt/python/cp310-cp310/bin/pip install .
+/opt/python/cp310-cp310/bin/pip wheel . -w ./dist --no-deps
 find . -type f -iname "*-linux*.whl" -execdir sh -c "auditwheel repair '{}' -w ./ --plat 'manylinux2014_x86_64' || { echo 'Repairing wheels failed.'; auditwheel show '{}'; exit 1; }" \;
 echo "Succesfully built wheels:"
 find . -type f -iname "*-manylinux*.whl"
