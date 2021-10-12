06c508a8ccb6db2571f7674e343551a9c2f1f756
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-10-12 11:27:48 +1100

    Show the configuration in the header

--- Tests/benchmarks/suite.py
+++ Tests/benchmarks/suite.py
@@ -9,9 +9,11 @@ from rich.console import Console
 from rich.table import Table
 from rich.text import Text
 
+REPEAT = 5
+TIMES = 5
 
 if __name__ == "__main__":
-    table = Table(title="Pyjion Benchmark Suite")
+    table = Table(title=f"Pyjion Benchmark Suite, repeat={REPEAT}, number={TIMES}")
 
     table.add_column("Benchmark", justify="right", style="cyan", no_wrap=True)
     table.add_column("Min", style="magenta")
@@ -27,17 +29,19 @@ if __name__ == "__main__":
         if hasattr(i, "__benchmarks__"):
             for benchmark in i.__benchmarks__:
                 func, desc, settings = benchmark
-                without_result = timeit.repeat(func, repeat=5, number=5)
+                without_result = timeit.repeat(func, repeat=REPEAT, number=TIMES)
                 pyjion.enable()
                 pyjion.config(**settings)
-                with_result = timeit.repeat(func, repeat=5, number=5)
+                with_result = timeit.repeat(func, repeat=REPEAT, number=TIMES)
                 pyjion.disable()
                 delta = (abs(fmean(with_result) - fmean(without_result)) / fmean(without_result)) * 100.0
                 if fmean(with_result) < fmean(without_result):
                     delta_repr = Text(f"{delta:.2f}%", style="green")
                 else:
                     delta_repr = Text(f"-{delta:.2f}%", style="red")
-                table.add_row(desc, "{:.2f}".format(min(without_result)), "{:.2f}".format(max(without_result)), "{:.2f}".format(fmean(without_result)), "{:.2f}".format(min(with_result)), "{:.2f}".format(max(with_result)), "{:.2f}".format(fmean(with_result)), delta_repr)
+                table.add_row(desc, "{:.2f}".format(min(without_result)), "{:.2f}".format(max(without_result)),
+                              "{:.2f}".format(fmean(without_result)), "{:.2f}".format(min(with_result)),
+                              "{:.2f}".format(max(with_result)), "{:.2f}".format(fmean(with_result)), delta_repr)
 
     console = Console()
     console.print(table)
