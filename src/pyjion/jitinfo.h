12fe440bd4fab7e2078dd01118b4c90f447cdf24
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-10-14 20:12:32 +1100

    Add windows ARM64 cross-compile support

--- src/pyjion/jitinfo.h
+++ src/pyjion/jitinfo.h
@@ -288,18 +288,13 @@ public:
 #ifdef _TARGET_ARM64_
             case IMAGE_REL_ARM64_BRANCH26:// 26 bit offset << 2 & sign ext, for B and BL
             {
-                _ASSERTE(slot == 0);
-                _ASSERTE(addlDelta == 0);
 
                 auto branchTarget = (uint64_t) target;
-                _ASSERTE((branchTarget & 0x3) == 0);// the low two bits must be zero
 
                 auto fixupLocation = (uint64_t) location;
                 auto fixupLocationRW = (uint64_t) locationRW;
-                _ASSERTE((fixupLocation & 0x3) == 0);// the low two bits must be zero
 
                 delta = (int64_t) (branchTarget - fixupLocation);
-                _ASSERTE((delta & 0x3) == 0);// the low two bits must be zero
 
                 // PutArm64Rel28
                 auto pCode = (uint32_t*) fixupLocationRW;
@@ -310,8 +305,6 @@ public:
             } break;
 
             case IMAGE_REL_ARM64_PAGEBASE_REL21: {
-                _ASSERTE(slot == 0);
-                _ASSERTE(addlDelta == 0);
 
                 // Write the 21 bits pc-relative page address into location.
                 int64_t targetPage = (int64_t) target & 0xFFFFFFFFFFFFF000LL;
@@ -329,8 +322,6 @@ public:
             } break;
 
             case IMAGE_REL_ARM64_PAGEOFFSET_12A: {
-                _ASSERTE(slot == 0);
-                _ASSERTE(addlDelta == 0);
 
                 // Write the 12 bits page offset into location.
                 uint32_t imm12 = (int32_t) (uint64_t) target & 0xFFFLL;
