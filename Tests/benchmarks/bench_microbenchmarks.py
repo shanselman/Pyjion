28198cee58891fbcfe5dd6773fc064f9c95c2512
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-10-17 19:22:26 +1100

    Add a bigint benchmark and a function call benchmark

--- Tests/benchmarks/bench_microbenchmarks.py
+++ Tests/benchmarks/bench_microbenchmarks.py
@@ -1,8 +1,3 @@
-import pyjion
-import timeit
-from statistics import fmean
-
-
 def test_floats(n=10000):
     for y in range(n):
         x = 0.1
@@ -17,4 +12,23 @@ def test_ints(n=10000):
         x *= z
 
 
-__benchmarks__ = [(test_floats, "floatmath_micro", {"level": 2}), (test_ints, "intmath_micro", {"level": 2, "pgc": True})]
+def test_bigints(n=10000):
+    for _ in range(n):
+        x = 200_100_100_100_100_100_100_100_100
+        y = 100_100_100_100_100_100_100_100_100
+        z = y * y + x - y
+        x *= z
+
+
+def test_function_calls(n=10000):
+    def f():
+        pass
+    for _ in range(n):
+        f()
+
+
+__benchmarks__ = [
+    (test_floats, "floatmath_micro", {"level": 2}),
+    (test_ints, "intmath_micro", {"level": 2, "pgc": True}),
+    (test_bigints, "bigintmath_micro", {"level": 2, "pgc": True}),
+    (test_function_calls, "function_call_micro", {"level": 2, "pgc": True})]
