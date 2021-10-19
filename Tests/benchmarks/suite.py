776404d2eca4a90fe474eb9c44cd7b5e06c16a00
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-10-19 15:46:21 +1100

    Improve the suite output

--- Tests/benchmarks/suite.py
+++ Tests/benchmarks/suite.py
@@ -24,13 +24,12 @@ if __name__ == "__main__":
 
     table.add_column("Benchmark", justify="right", style="cyan", no_wrap=True)
     table.add_column("Repeat", style="magenta")
-    table.add_column("Min", style="magenta", width=10)
-    table.add_column("Max", style="magenta", width=10)
-    table.add_column("Mean", style="magenta", width=10)
-    table.add_column("Min (+)", style="blue", width=10)
-    table.add_column("Max (+)", style="blue", width=10)
-    table.add_column("Mean (+)", style="blue", width=10)
-    table.add_column("Delta", justify="right", style="green", width=10)
+    table.add_column("Min", style="magenta", width=7)
+    table.add_column("Max", style="magenta", width=7)
+    table.add_column("Mean", style="magenta", width=7)
+    table.add_column("Min (+)", style="blue", width=15)
+    table.add_column("Max (+)", style="blue", width=15)
+    table.add_column("Mean (+)", style="blue", width=15)
 
     graphs_out = pathlib.Path(__file__).parent / 'graphs'
     if not graphs_out.exists():
@@ -74,14 +73,34 @@ if __name__ == "__main__":
                                     dot.write(g)
                                     dot.write('')
 
-                delta = (abs(fmean(with_result) - fmean(without_result)) / fmean(without_result)) * 100.0
-                if fmean(with_result) < fmean(without_result):
-                    delta_repr = Text(f"{delta:.3f}%", style="green")
+                delta_mean = (abs(fmean(with_result) - fmean(without_result)) / fmean(without_result)) * 100.0
+                delta_min = (abs(min(with_result) - min(without_result)) / min(without_result)) * 100.0
+                delta_max = (abs(max(with_result) - max(without_result)) / max(without_result)) * 100.0
+
+                if min(with_result) < min(without_result):
+                    fdelta_min = Text(f"{min(with_result):.2f} ({delta_min:.1f}%)", style="green")
                 else:
-                    delta_repr = Text(f"-{delta:.1f}%", style="red")
-                table.add_row(desc, str(repeat), "{:.3f}".format(min(without_result)), "{:.3f}".format(max(without_result)),
-                              "{:.3f}".format(fmean(without_result)), "{:.3f}".format(min(with_result)),
-                              "{:.3f}".format(max(with_result)), "{:.3f}".format(fmean(with_result)), delta_repr)
+                    fdelta_min = Text(f"{min(with_result):.2f} (-{delta_min:.1f}%)", style="red")
+
+                if max(with_result) < max(without_result):
+                    fdelta_max = Text(f"{max(with_result):.2f} ({delta_max:.3f}%)", style="green")
+                else:
+                    fdelta_max = Text(f"{max(with_result):.2f} (-{delta_max:.1f}%)", style="red")
+
+                if fmean(with_result) < fmean(without_result):
+                    fdelta_mean = Text(f"{fmean(with_result):.2f} ({delta_mean:.1f}%)", style="green")
+                else:
+                    fdelta_mean = Text(f"{fmean(with_result):.2f} (-{delta_mean:.1f}%)", style="red")
+
+                table.add_row(desc,
+                              str(repeat),
+                              "{:.3f}".format(min(without_result)),
+                              "{:.3f}".format(max(without_result)),
+                              "{:.3f}".format(fmean(without_result)),
+                              fdelta_min,
+                              fdelta_max,
+                              fdelta_mean,
+                              )
 
     console = Console(width=150)
     console.print(table)
