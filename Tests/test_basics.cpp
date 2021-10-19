5a66adf1d9bf8e9e9372fd41cce123701817744e
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-10-19 14:19:45 +1100

    Move the global tests

--- Tests/test_basics.cpp
+++ Tests/test_basics.cpp
@@ -599,65 +599,6 @@ TEST_CASE("Sequence binary operations") {
     }
 }
 
-TEST_CASE("Test builtins") {
-    SECTION("call print()") {
-        auto t = EmissionTest("def f(): return print('hello world')");
-        CHECK(t.returns() == "None");
-    }
-    SECTION("call ord()") {
-        auto t = EmissionTest("def f(): return ord('a')");
-        CHECK(t.returns() == "97");
-    }
-    SECTION("call int()") {
-        auto t = EmissionTest("def f(): return int('97')");
-        CHECK(t.returns() == "97");
-    }
-    SECTION("call min()") {
-        auto t = EmissionTest("def f(): return min([100, 101, 97])");
-        CHECK(t.returns() == "97");
-    }
-    SECTION("call min() again") {
-        auto t = EmissionTest("def f(): return min(100, 101, 97)");
-        CHECK(t.returns() == "97");
-    }
-    SECTION("call type()") {
-        auto t = EmissionTest("def f(): return type(2)");
-        CHECK(t.returns() == "<class 'int'>");
-    }
-    SECTION("call type() more complicatedly?") {
-        auto t = EmissionTest("def f(): return isinstance(type(2), type)");
-        CHECK(t.returns() == "True");
-    }
-    SECTION("call max() and map()") {
-        auto t = EmissionTest("def f(): args=('a', 'aaa', 'aaaaa'); return max(map(len, args))");
-        CHECK(t.returns() == "5");
-    }
-    SECTION("call chr()") {
-        auto t = EmissionTest("def f(): return chr(32) * 10");
-        CHECK(t.returns() == "'          '");
-    }
-    SECTION("call bin()") {
-        auto t = EmissionTest("def f(): return bin(2**6)");
-        CHECK(t.returns() == "'0b1000000'");
-    }
-    SECTION("call bin() with a big number!") {
-        auto t = EmissionTest("def f(): return bin(2**65)");
-        CHECK(t.returns() == "'0b100000000000000000000000000000000000000000000000000000000000000000'");
-    }
-    SECTION("call reversed() ") {
-        auto t = EmissionTest("def f(): return tuple(reversed((1,2,3,4)))");
-        CHECK(t.returns() == "(4, 3, 2, 1)");
-    }
-    SECTION("call bytearray() ") {
-        auto t = EmissionTest("def f(): return bytearray((1,2,3,4))");
-        CHECK(t.returns() == "bytearray(b'\\x01\\x02\\x03\\x04')");
-    }
-    SECTION("call bytearray() with dud data ") {
-        auto t = EmissionTest("def f(): return bytearray(None)");
-        CHECK(t.raises() == PyExc_TypeError);
-    }
-}
-
 TEST_CASE("Test type annotations") {
     SECTION("test variable definition with annotations") {
         auto t = EmissionTest(
