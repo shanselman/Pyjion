7de3a54e08a14523a4a0a47618c328deec268285
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-12-23 14:15:34 +1100

    add asan for windows support, report load address on pyjion

--- src/pyjion/__init__.py
+++ src/pyjion/__init__.py
@@ -5,13 +5,15 @@ import platform
 from enum import IntFlag, IntEnum
 from dataclasses import dataclass
 
-__version__ = '1.2.1'
+__version__ = "1.2.1"
 
 
 def _no_dotnet(path):
-    raise ImportError(f"Can't find a .NET 6 installation in {path}, "
-                      "provide the DOTNET_ROOT environment variable "
-                      "if it's installed somewhere unusual")
+    raise ImportError(
+        f"Can't find a .NET 6 installation in {path}, "
+        "provide the DOTNET_ROOT environment variable "
+        "if it's installed somewhere unusual"
+    )
 
 
 def _which_dotnet() -> str:
@@ -19,19 +21,21 @@ def _which_dotnet() -> str:
     Locate the clrjit library path
     """
     _dotnet_root = None
-    if 'DOTNET_ROOT' in os.environ:
-        _dotnet_root = pathlib.Path(os.environ['DOTNET_ROOT'])
+    if "DOTNET_ROOT" in os.environ:
+        _dotnet_root = pathlib.Path(os.environ["DOTNET_ROOT"])
         if not _dotnet_root.exists():
             _no_dotnet(_dotnet_root)
-    if 'DOTNET_LIB_PATH' in os.environ:
-        ctypes.cdll.LoadLibrary(os.environ['DOTNET_LIB_PATH'])
-        return os.environ['DOTNET_LIB_PATH']
+    if "DOTNET_LIB_PATH" in os.environ:
+        ctypes.cdll.LoadLibrary(os.environ["DOTNET_LIB_PATH"])
+        return os.environ["DOTNET_LIB_PATH"]
     if platform.system() == "Darwin":
         if not _dotnet_root:
-            _dotnet_root = pathlib.Path('/usr/local/share/dotnet/')
+            _dotnet_root = pathlib.Path("/usr/local/share/dotnet/")
             if not _dotnet_root.exists():
                 _no_dotnet(_dotnet_root)
-        lib_path = list(_dotnet_root.glob('shared/Microsoft.NETCore.App*/6.0.*/libclrjit.dylib'))
+        lib_path = list(
+            _dotnet_root.glob("shared/Microsoft.NETCore.App*/6.0.*/libclrjit.dylib")
+        )
         if len(lib_path) > 0:
             clrjitlib = str(lib_path[0])
             ctypes.cdll.LoadLibrary(clrjitlib)
@@ -40,7 +44,10 @@ def _which_dotnet() -> str:
             _no_dotnet(_dotnet_root)
     elif platform.system() == "Linux":
         if not _dotnet_root:
-            search_paths = [pathlib.Path('/usr/local/share/dotnet/'), pathlib.Path('/usr/share/dotnet/')]
+            search_paths = [
+                pathlib.Path("/usr/local/share/dotnet/"),
+                pathlib.Path("/usr/share/dotnet/"),
+            ]
             for path in search_paths:
                 if not path.exists():
                     continue
@@ -48,7 +55,9 @@ def _which_dotnet() -> str:
                     _dotnet_root = path
         if not _dotnet_root:
             _no_dotnet(_dotnet_root)
-        lib_path = list(_dotnet_root.glob('shared/Microsoft.NETCore.App*/6.0.*/libclrjit.so'))
+        lib_path = list(
+            _dotnet_root.glob("shared/Microsoft.NETCore.App*/6.0.*/libclrjit.so")
+        )
         if len(lib_path) > 0:
             clrjitlib = str(lib_path[0])
             ctypes.cdll.LoadLibrary(clrjitlib)
@@ -57,10 +66,14 @@ def _which_dotnet() -> str:
             _no_dotnet(_dotnet_root)
     elif platform.system() == "Windows":
         if not _dotnet_root:
-            _dotnet_root = pathlib.WindowsPath(os.path.expandvars(r'%ProgramFiles%\dotnet'))
+            _dotnet_root = pathlib.WindowsPath(
+                os.path.expandvars(r"%ProgramFiles%\dotnet")
+            )
             if not _dotnet_root.exists():
                 _no_dotnet(_dotnet_root)
-        lib_path = list(_dotnet_root.glob('shared/Microsoft.NETCore.App*/6.0.*/clrjit.dll'))
+        lib_path = list(
+            _dotnet_root.glob("shared/Microsoft.NETCore.App*/6.0.*/clrjit.dll")
+        )
         if len(lib_path) > 0:
             clrjitlib = str(lib_path[0])
             ctypes.cdll.LoadLibrary(clrjitlib)
@@ -74,14 +87,25 @@ def _which_dotnet() -> str:
 lib_path = _which_dotnet()
 
 try:
-    from ._pyjion import enable, disable, info as _info, il, native, offsets, \
-        graph, init as _init, symbols, config, PyjionUnboxingError
+    from ._pyjion import (
+        enable,
+        disable,
+        info as _info,
+        il,
+        native,
+        offsets,
+        graph,
+        init as _init,
+        symbols,
+        config,
+        PyjionUnboxingError,
+    )
 
     _init(lib_path)
-except ImportError:
+except ImportError as i:
     raise ImportError(
-        """
-Failed to import the compiled Pyjion module. This normally means something went wrong during pip install
+        f"""
+Failed to import the compiled Pyjion module ({i.path}). This normally means something went wrong during pip install
 and the binaries weren't compiled. Make sure you update pip before installing to get the right wheel.
 If that doesn't work, run pip in verbose mode, or file an issue at https://github.com/tonybaloney/pyjion/.
 """
@@ -108,8 +132,8 @@ class OptimizationFlags(IntFlag):
 
 
 class CompilationResult(IntEnum):
-    NoResult = 0,
-    Success = 1,
+    NoResult = (0,)
+    Success = (1,)
     CompilationException = 10
     CompilationJitFailure = 11
     CompilationStackEffectFault = 12
@@ -142,11 +166,13 @@ class JitInfo:
 
 def info(f) -> JitInfo:
     d = _info(f)
-    return JitInfo(d['failed'],
-                   CompilationResult(d['compile_result']),
-                   d['compiled'],
-                   OptimizationFlags(d['optimizations']),
-                   PgcStatus(d['pgc']),
-                   d['run_count'],
-                   d['tracing'],
-                   d['profiling'])
+    return JitInfo(
+        d["failed"],
+        CompilationResult(d["compile_result"]),
+        d["compiled"],
+        OptimizationFlags(d["optimizations"]),
+        PgcStatus(d["pgc"]),
+        d["run_count"],
+        d["tracing"],
+        d["profiling"],
+    )
