0d78fc1722e5f64cb7c8af92bc11bb2f7d1f23a4
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-02-09 12:46:58 +1100

    Add a setup script

---
+++ Tests/fuzzing/setup/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DOTNET_VERSION=6.0.101
+export TZ=Europe/London
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+apt-get -y update && apt-get install -y software-properties-common && apt-get -y update \
+    && add-apt-repository ppa:deadsnakes/ppa && apt-get -y update \
+    && apt-get install -y wget bison unzip bzip2 
+    && apt-get install -y python3.10 python3.10-dev python3.10-distutils python3.10-venv \
+    && apt-get clean -y && rm -rf /var/lib/apt/lists/*
+wget https://dotnetcli.azureedge.net/dotnet/Sdk/${DOTNET_VERSION}/dotnet-sdk-${DOTNET_VERSION}-linux-x64.tar.gz
+mkdir -p dotnet && tar zxf dotnet-sdk-${DOTNET_VERSION}-linux-x64.tar.gz -C dotnet
