95f65c515d4252f5c58709dc95b7c587366d1113
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-02-11 14:54:23 +1100

    Improve the fuzzer to compile in Pyjion as well

--- Tests/fuzzing/fuzz_target.cpp
+++ Tests/fuzzing/fuzz_target.cpp
@@ -45,13 +45,10 @@ bool InitializePyjion() {
     return true;
 }
 
-PyCodeObject* CompileCode(const char* code) {
-    auto globals = PyObject_ptr(PyDict_New());
-
+PyCodeObject* CompileCode(const char* code, py_ptr<PyObject> globals, py_ptr<PyObject> locals) {
     auto builtins = PyEval_GetBuiltins();
     PyDict_SetItemString(globals.get(), "__builtins__", builtins);
 
-    auto locals = PyObject_ptr(PyDict_New());
     PyRun_String(code, Py_file_input, globals.get(), locals.get());
     if (PyErr_Occurred()) {
         PyErr_Clear();
@@ -64,9 +61,50 @@ PyCodeObject* CompileCode(const char* code) {
     return (PyCodeObject*) PyObject_GetAttrString(func.get(), "__code__");;
 }
 
+PyObject* run(PyjionJittedCode* jittedcode, PyCodeObject* code, py_ptr<PyObject> globals, py_ptr<PyObject> locals) {
+    auto sysModule = PyObject_ptr(PyImport_ImportModule("sys"));
+    auto builtins = PyEval_GetBuiltins();
+    PyDict_SetItemString(globals.get(), "__builtins__", builtins);
+    PyDict_SetItemString(globals.get(), "sys", sysModule.get());
+    auto profile = new PyjionCodeProfile();
+    auto tstate = PyThreadState_Get();
+
+    auto frame = PyFrame_New(tstate, code, globals.get(), locals.get());
+    auto prev = _PyInterpreterState_GetEvalFrameFunc(PyInterpreterState_Main());
+    _PyInterpreterState_SetEvalFrameFunc(PyInterpreterState_Main(), PyJit_EvalFrame);
+    auto res = PyJit_ExecuteAndCompileFrame(jittedcode, frame, tstate, profile);
+    assert(frame->f_stackdepth != -1);
+    assert(frame->f_lasti >= 0);
+    assert(frame->f_lasti * 2 < PyBytes_GET_SIZE(code->co_code));
+    _PyInterpreterState_SetEvalFrameFunc(PyInterpreterState_Main(), prev);
+    Py_DECREF(frame);
+    PyGC_Collect();
+    assert(!jittedcode->j_failed);
+    assert(jittedcode->j_genericAddr != nullptr);
+    delete profile;
+    return res;
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  static bool Initialized = InitializePyjion();
-  auto input = string(reinterpret_cast<const char*>(Data), Size);
-  CompileCode(input.c_str());
+    static bool Initialized = InitializePyjion();
+    auto input = string(reinterpret_cast<const char*>(Data), Size);
+    
+    auto globals = PyObject_ptr(PyDict_New());
+    auto locals = PyObject_ptr(PyDict_New());
+    auto co = CompileCode(input.c_str(), globals, locals);
+    if (co == nullptr) {
+    return 0;
+    }
+    auto jitted = PyJit_EnsureExtra((PyObject*) co);
+    auto res = PyObject_ptr(run(jitted, co, globals, locals));
+    if (res.get() == nullptr || PyErr_Occurred()) {
+        PyErr_PrintEx(-1);
+        PyErr_Clear();
+        return 0;
+    }
+
+    auto repr = PyUnicode_AsUTF8(PyObject_Repr(res.get()));
+    printf("Returned: %s \n", repr);
+
   return 0;  // Non-zero return values are reserved for future use.
 }
